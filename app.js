0a24c03e65cf48b9e360fea9933f416749ec2c5f
cmoinr <cmoinieves@gmail.com>
2024-11-09 19:17:39 -0400

    main01

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-// view engine setup
+// Configuración del motor de plantillas EJS
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Servir archivos estáticos
+// Configurar la ruta a los archivos estáticos usando una variable de entorno
+const staticPath = process.env.STATIC_PATH || 'public';
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(express.static(path.join(__dirname, staticPath)));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Rutas
+app.get('/', (req, res) => {
+    res.render('index'); // Renderiza la vista index.ejs
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/music', (req, res) => {
+    res.render('music');
 });
 
-module.exports = app;
+app.get('/programming', (req, res) => {
+  res.render('programming');
+});
+
+app.get('/sports', (req, res) => {
+  res.render('sports');
+});
+
+app.get('/aboutme', (req, res) => {
+  res.render('aboutme');
+});
+
+// Puerto de escucha
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Servidor escuchando en el puerto ${port}`);
+});
