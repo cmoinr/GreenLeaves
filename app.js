b2a59eed6cb43944ac7ae5c66525d61f52091b9e
cmoinr <cmoinieves@gmail.com>
2024-11-29 13:33:22 -0400

    main22

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const contactosController = require('./contacts_controller');
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 
@@ -13,8 +13,29 @@ app.set('view engine', 'ejs');
 const staticPath = process.env.STATIC_PATH || 'public';
 
 app.use(express.static(path.join(__dirname, staticPath)));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+class ContactsModel {
+  constructor() {
+      this.db = new sqlite3.Database('db.sqlite');
+      this.db.run('CREATE TABLE IF NOT EXISTS contacts (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, name TEXT, message TEXT, ip TEXT, date TEXT)');
+  }
+
+  async save(email, name, message, ip, date) {
+      return new Promise((resolve, reject) => {
+          this.db.run('INSERT INTO contacts (email, name, message, ip, date) VALUES (?, ?, ?, ?, ?)', [email, name, message, ip, date], (err) => {
+              if (err) {
+                  reject(err);
+              } else {
+                  resolve();
+              }
+          });
+      });
+  }
+}
+
+const contactosModel = new ContactsModel();
 
 // Rutas
 app.get('/', (req, res) => {
@@ -33,17 +54,43 @@ app.get('/services', (req, res) => {
   res.render('services');
 });
 
-app.get('/contact', (req, res) => {
-  res.render('contact');
-});
-
-app.use('/contact', contactosController);
-
 app.get('/thanks', (req, res) => {
   res.send('¡Gracias por tu mensaje!');
 });
 
-// Puerto de escucha
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.post('/send', async (req, res) => {
+
+  try {
+      // Obtener direccion IPv4
+      const publicIp = await import('public-ip');
+      const ip = await publicIp.v4();
+      
+      const { email, name, message } = req.body;
+      const date = new Date().toISOString();
+
+        // Validación básica (puedes agregar más validaciones)
+      if (!email || !name || !message) {
+          return res.status(400).send('Por favor, completa todos los campos');
+      }
+
+      try {
+          await contactosModel.save(email, name, message, ip, date);
+          res.redirect('/thanks');
+      } catch (error) {
+          console.error(error);
+          res.status(500).send('Error al guardar los datos');
+      }
+  } catch (err) {
+      console.error(err);
+      res.status(500).send('Error al obtener la dirección IP');
+  }
+});
+
+// Puerto
 const port = 3000;
 app.listen(port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`);
