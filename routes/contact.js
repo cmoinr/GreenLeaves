cf4fdfc96c3d475ecd8ec69e7e4de1d48dae0eaa
cmoinr <cmoinieves@gmail.com>
2025-01-07 18:27:55 -0400

    main35

--- routes/contact.js
+++ routes/contact.js
@@ -2,98 +2,66 @@ const express = require('express');
 const router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
 const axios = require('axios');
-// const nodemailer = require('nodemailer');
-// const passport = require('passport');
-// const GoogleStrategy = require('passport-google-oauth2').Strategy;
-// const OAuth2 = require('nodemailer-oauth2');
-
-// testing this
-const createAssessment = require('./recaptcha');
+const nodemailer = require('nodemailer');
 
 // Contacto (formulario)
 router.get('/contact', (req, res) => {
     res.render('contact');
 });
 
-// passport.use(new GoogleStrategy({
-//     clientID: "48331960941-dcevsjrp1m86ete3at04fiqc24hdl40s.apps.googleusercontent.com",
-//     clientSecret: "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",
-//     callbackURL: "http://localhost:2700/send"
-//   },
-//   function(accessToken, refreshToken, profile, done) {
-//     // Aquí puedes guardar la información del usuario en tu base de datos, si es necesario
-//     return done(null, profile);
-//   }
-// ));
-
-// let oauth2 = new OAuth2({
-//     user: 'cmoinieves@gmail.com', // Utiliza el correo asociado a tu proyecto de GCP
-//     clientId: "48331960941-dcevsjrp1m86ete3at04fiqc24hdl40s.apps.googleusercontent.com",
-//     clientSecret: "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",
-//     refreshToken: REFRESH_TOKEN, // Obtendrás este token después del primer inicio de sesión
-//     accessTokenUri: 'https://oauth2.googleapis.com/token'
-// });
-
-// let transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         type: 'OAuth2',
-//         user: 'cmoinieves@gmail.com',
-//         clientId: "48331960941-dcevsjrp1m86ete3at04fiqc24hdl40s.apps.googleusercontent.com",
-//         clientSecret: "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",
-//         refreshToken: REFRESH_TOKEN,
-//         accessToken: oauth2.accessToken
-//     }
-// });
-
-// const transporter = nodemailer.createTransport({
-//     host: 'smtp.gmail.com',
-//     port: 587,
-//     secure: false,
-//     auth: {
-//         user: 'cmoinr@hotmail.com',
-//         pass: 'hell0Moises-'
-//     }
-// });
-
 // Funcion para verificar el token (reCAPTCHA)
-const verifyCaptcha = async (token) => {
-    const secretKey = '6LfnUq0qAAAAALB3Y3DtLjM8YoLFXuO4beUl0A1L';
-    const verifyUrl = `https://www.google.com/recaptcha/api/siteverify`; // ?secret=${secretKey}&response=${token}
+const verifyCaptcha = async (token, ip) => {
+    const secretKey = '6Let8K8qAAAAANUHKjjIg1SgDjrQGpY-Xrx0kKj8';
+    const verify = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}&remoteip=${ip}`;
 
     try {
-        const response = await axios.post(verifyUrl, {
-            secret: secretKey,
-            response: token,
-        });
-
-        if (response.data.success) {
+        const recaptcha = await axios.post(verify);
+        if (recaptcha.data.success) {
             return true;
         } else {
             return false;
         }
-
     } catch (error) {
-        console.error('Error al verificar el reCAPTCHA:', error);
         return false;
     }
+};
 
-    // request(verifyUrl, (err, response, body) => {
-    //     if(err) {
-    //         console.log(err);
-    //     }
+// Funcion para el envio del correo electronico
+const sendEmail = async (name, email, message, userIP, country, date) => {    
+    // Configurar el transporte del correo electronico
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: "cmoinieves@gmail.com",
+          pass: "hell0Moises-"
+        }
+      });
 
-    //     body = JSON.parse(body);
+    // Construccion del correo electronico
+    const mailData = {
+        from: 'cmoinieves@gmail.com',
+        to: ['programacion2ais@dispostable.com', 'cmoinr@hotmail.com'],
+        subject: 'GreenLeaves | Formulario de contacto',
+        text: `
+            Nombre:     ${name}
+            Correo:     ${email}
+            Comentario: ${message}
+            IP:         ${userIP}
+            País:       ${country}
+            Fecha/Hora: ${date}
+        `
+    };
 
-    //     if(!body.success && body.success === undefined) {
-    //         return res.json({"success":false, "msg":"captcha verification failed"});
-    //     }
-    //     else if(body.score < 0.5) {
-    //         return res.json({"success":false, "msg":"you might be a bot, sorry!", "score": body.score});
-    //     }        
-    //     // return json message or continue with your function. Example: loading new page, ect
-    //     return res.json({"success":true, "msg":"captcha verification passed", "score": body.score});
-    // })
+    // Envio del correo electronico   
+    transporter.sendMail(mailData, (error, info) => {
+        if (error) {
+            console.error('Error al enviar el correo electrónico:', error);
+            return false;
+        } else {
+            console.log('Correo electrónico enviado:', info.response);
+            return true;
+        }         
+    })
 };
 
 // ContactsModel
@@ -135,83 +103,36 @@ const contactosModel = new ContactsModel();
 // ContactsController
 router.post('/send', async (req, res) => {
     // Datos obtenidos del formulario
-    const { email, name, message, userIP, 'g-recaptcha-response': token } = req.body;
+    const { email, name, message, userIP, "g-recaptcha-response": token } = req.body;
     const date = new Date().toISOString();
 
     // Uso de la API [ipstack.com] (geolocalización por IP)
     const response = await axios.get(`http://api.ipstack.com/${userIP}?access_key=f8ff13db27bbc910d87fe504f4c6260e`);
     const country = response.data.country_name;
 
-    console.log(req.body)
-
-    // Validar el token de reCAPTCHA
-    if(!token) {
-        return res.status(400).send('reCAPTCHA error');
-    }
-
     // Validar los datos del formulario antes de guardarlos
-    if (!email || !name || !message) {
-        return res.status(400).send('Por favor, completa todos los campos');
+    if (!email || !name || !message || !token) {
+        return res.status(400).send('Por favor, completa todos los campos (incluyendo el reCAPTCHA)');
     }
 
-    // Construccion del correo electronico
-    // const mailData = {
-    //     from: 'GreenLeaves <cmoinieves@gmail.com>',
-    //     to: ['programacion2ais@dispostable.com'],
-    //     subject: 'GreenLeaves | Formulario de contacto',
-    //     text: `
-    //         Nombre: ${name}
-    //         Correo: ${email}
-    //         Comentario: ${message}
-    //         IP: ${userIP}
-    //         País: ${country}
-    //         Fecha/Hora: ${date}
-    //     `
-    // };
+    // Verificar la validez del token (reCAPTCHA)
+    if (verifyCaptcha(token, userIP)) {
 
-    try {
-        // const verificationResult = await createAssessment({
-        //     token,
-        //     recaptchaAction: 'submit',
-        //     // ... otros parámetros si es necesario
-        // });
-
-        if (await verifyCaptcha(token)) { // verificationResult.success || 
-            // El token es válido, procesar el formulario
+        // Verificar el envio del correo electronico
+        if (sendEmail(name, email, message, userIP, country, date)) {
 
             // Llamar a la clase ContactosModel para guardar los datos
             await contactosModel.save(email, name, message, userIP, date, country);
 
-            // Envio del correo electronico
-            // transporter.sendMail(mailData, (error, info) => {
-            //     if (error) {
-            //         console.log(error);
-            //     } else {
-            //         console.log('Email enviado: ' + info.response);
-            //     }
-            // });
-
             // Redireccionar al usuario a una página de confirmación o mostrar un mensaje de éxito
             res.redirect('/thanks');
-        } else {
-            res.status(400).send('Error al verificar el reCAPTCHA');
-        }
-    } catch (error) {
-        console.error('Error al verificar el reCAPTCHA:', error);
-        res.status(500).send('Error del servidor');
-    }
 
-    // if (await verifyCaptcha(token)) {
-    //     // Procesar los datos del formulario
-    //     try {
-                 
-    //     } catch (error) {
-    //         console.error(error);
-    //         res.status(500).send('Error al guardar los datos');
-    //     }   
-    // } else {
-    //     res.status(400).send('Por favor, completa el reCAPTCHA');
-    // }
+        } else {
+            res.status(400).send('Error Email: no se pudo enviar');
+        }
+    } else {
+        res.status(400).send('reCAPTCHA: error de validacion');
+    }
 });
 
 
@@ -222,7 +143,7 @@ contactosModel.get_info()
         console.log(contacts);
     })
     .catch(err => {
-        console.error('Error retrieving contacts:', err);
+        console.error('Error:', err);
     });
 
 
